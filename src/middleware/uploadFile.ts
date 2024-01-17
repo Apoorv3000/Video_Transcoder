f776021c3512ccec87ddc03f45de4830f8155d91
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2024-01-17 13:01:39 +0530

    added video upload and setup for the transcoder

--- src/middleware/uploadFile.ts
+++ src/middleware/uploadFile.ts
@@ -1,13 +1,38 @@
 import { NextFunction, Request, Response } from "express";
-import multer from "multer";
+import multer, { Multer } from "multer";
+import path from "path";
+import fs from "fs";
 
-export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
-	const upload = multer.diskStorage({
-		destination: (req, file, cb) => {
-			cb(null, "./src/uploads");
-		},
-		filename: (req, file, cb) => {
-			cb(null, file.originalname);
-		},
-	});
+const getUploadDirectoryPath = (filename: string) => {
+	return path.join(__dirname, "../uploads", filename);
 };
+
+const ensureUploadDirPathExist = (filename: string) => {
+	const dirPath = getUploadDirectoryPath(filename);
+	if (!fs.existsSync(dirPath)) {
+		fs.mkdirSync(dirPath);
+	}
+};
+
+const storage = multer.diskStorage({
+	destination: (req: Request, file: Express.Multer.File, cb) => {
+		const fileSpecificFolder = file.originalname.split(path.extname(file.originalname))[0];
+		ensureUploadDirPathExist(fileSpecificFolder);
+		cb(null, getUploadDirectoryPath(fileSpecificFolder));
+	},
+	filename: (req: Request, file: Express.Multer.File, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+export const uploadFile: Multer = multer({
+	storage,
+	fileFilter: (req, file, cb) => {
+		const allowedFileTypes = [".mp4"];
+		if (allowedFileTypes.includes(path.extname(file.originalname))) {
+			cb(null, true);
+		} else {
+			cb(new Error("File type not allowed"));
+		}
+	},
+});
--- src/services/videoTranscoder.ts
+++ src/services/videoTranscoder.ts
@@ -1,1 +1,11 @@
-export class videoTranscoder {}
+import ffmpeg from "fluent-ffmpeg";
+export class videoTranscoder {
+	private outputFile: string;
+	private inputFile: string;
+	constructor(outputFile: string, inputFile: string) {
+		this.outputFile = outputFile;
+		this.inputFile = inputFile;
+	}
+
+	public static transcodeToHLS = (outputFile: string, inputFile: string) => {};
+}
