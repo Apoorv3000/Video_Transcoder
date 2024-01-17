23a4e5a18ba96e5375c40414883ad95e5117ff1a
Apoorv Kumar <apoorvkumarvbcv@gmail.com>
2024-01-17 13:28:25 +0530

    added video transcoder class which converts mp4 to hls and ts files

--- src/services/videoTranscoder.ts
+++ src/services/videoTranscoder.ts
@@ -1,11 +1,35 @@
 import ffmpeg from "fluent-ffmpeg";
 export class videoTranscoder {
-	private outputFile: string;
-	private inputFile: string;
-	constructor(outputFile: string, inputFile: string) {
-		this.outputFile = outputFile;
-		this.inputFile = inputFile;
+	private inputPath: string;
+	private outputPath: string;
+
+	constructor(inputPath: string, outputPath: string) {
+		this.inputPath = inputPath;
+		this.outputPath = outputPath;
 	}
 
-	public static transcodeToHLS = (outputFile: string, inputFile: string) => {};
+	public transcodeToHLS(outputOptions: string[], qualityLevels: { bitrate: string; resolution: string }[]): Promise<{ quality: { bitrate: string; resolution: string }; outputPath: string }[]> {
+		const promises: Promise<{ quality: { bitrate: string; resolution: string }; outputPath: string }>[] = [];
+
+		qualityLevels.forEach((quality, index) => {
+			const outputPath = `${this.outputPath}/output_${index}.m3u8`;
+
+			const promise = new Promise<{ quality: { bitrate: string; resolution: string }; outputPath: string }>((resolve, reject) => {
+				ffmpeg(this.inputPath)
+					.inputFormat("mp4")
+					.inputOptions(["-preset fast", "-tune film"])
+					.videoCodec("libx264")
+					.audioCodec("aac")
+					.outputOptions(outputOptions)
+					.output(outputPath)
+					.on("end", () => resolve({ quality, outputPath }))
+					.on("error", (err) => reject(err))
+					.run();
+			});
+
+			promises.push(promise);
+		});
+
+		return Promise.all(promises);
+	}
 }
